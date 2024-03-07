b267f0faf8e0d26fd02ae522600d8f379587a244
RaivoCh <honza.simurda@gmail.com>
2024-03-07 21:10:55 +0100

    array

--- 2AI_L7_v1.0.py
+++ 2AI_L7_v1.0.py
@@ -2,3 +2,14 @@
 #https://www.youtube.com/watch?v=W43MpRroplA&list=PLGs0VKk2DiYyXlbJVaE8y1qr24YldYNDm&index=10
 
 ##
+import cv2
+print(cv2.__version__)
+import numpy as np
+
+while True:
+    frame=np.zeros([250, 250],dtype=np.uint8)
+    frame[:125,125:255]=125
+    cv2.imshow("my window",frame)
+    if cv2.waitKey(1) & 0xff==ord("q"):
+        break
+        
