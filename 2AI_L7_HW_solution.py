57be1a3680705d428be7e6aa45f2de133ed1600a
Raivo <honza.simurda@gmail.com>
2024-03-11 19:03:47 +0100

    new lesson

---
+++ 2AI_L7_HW_solution.py
@@ -0,0 +1,8 @@
+#AI for Everyone LESSON 7: HOMEWORK SOLUTION, Creating Checkerboard Pattern in OPENCV
+#https://www.youtube.com/watch?v=Fpqa5mcHoTE&list=PLGs0VKk2DiYyXlbJVaE8y1qr24YldYNDm&index=16
+
+##HOMEWORK
+#chessboard
+#chessboar is square, so same number of rows and colums
+#homework solution
+
