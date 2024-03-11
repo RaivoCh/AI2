447ac805d85b043a138b6bffc58e7f39c58bd3f7
RaivoCh <honza.simurda@gmail.com>
2024-03-11 15:24:16 +0100

    v1.3 two for loops

--- 2AI_L7_HW_v1.0.py
+++ 2AI_L7_HW_v1.0.py
@@ -23,11 +23,18 @@ while True:
     frame=np.zeros([sizeB, sizeB,3],dtype=np.uint8)
 
     for i in range(0,row_col,2):
-        
+        for j in range(0,row_col,2):        
     #    frame[2*i*rowSize:rowSize+i*2*rowSize,2*i*rowSize:rowSize+i*2*rowSize]=(0,0,125)
-        frame[i*rowSize:i+1*rowSize,rowSize:]=(0,0,125)
+            frame[i*rowSize:(i+1)*rowSize,j*rowSize:(j+1)*rowSize]=(0,0,125)
     #    frame[:,2*i*rowSize:rowSize+i*2*rowSize]=(0,0,125)
         print(i)
+
+    for k in range(0,row_col,2):
+        for l in range(1,row_col,2):        
+    #    frame[2*i*rowSize:rowSize+i*2*rowSize,2*i*rowSize:rowSize+i*2*rowSize]=(0,0,125)
+            frame[K*rowSize:(k+1)*rowSize,l*rowSize:(l+1)*rowSize]=(0,0,125)
+
+
     
     cv2.imshow("my window",frame)
     if cv2.waitKey(1) & 0xff==ord("q"):
