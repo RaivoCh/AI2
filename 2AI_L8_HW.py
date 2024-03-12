35ddc27d16a0fb98c472a5190fd61cae78c4af4f
Raivo <honza.simurda@gmail.com>
2024-03-12 21:54:58 +0100

    homework

---
+++ 2AI_L8_HW.py
@@ -0,0 +1,6 @@
+# AI for Everyone LESSON 8: Putting Text, Rectangles and Circles on Images in OpenCV
+# https://www.youtube.com/watch?v=97HA3NeYjus&list=PLGs0VKk2DiYyXlbJVaE8y1qr24YldYNDm&index=12
+
+#homework - make framerate counter (frames per second) and display it in frame
+#hint: one whole loop is one processed frame
+
